9f5e29a88cb6a66d0c7a90df7848584c9b9e8bc3
cheyenneloh2025-sketch <cheyenne.loh.wenjun@gmail.com>
2026-07-06 15:18:47 +0800

    changed commit to call schedule

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import asyncio
 
 from telegram_bot import attendance_bot
+import threading
+from scheduler import scheduler
 
+def start_scheduler():
+    print("===================================")
+    print("Scheduler Started")
+    print("===================================")
+    scheduler.start()
 
 async def main():
 
     print("===================================")
     print("Attendance Bot Started")
     print("===================================")
+    threading.Thread(
+        target=start_scheduler,
+        daemon=True
+    ).start()
 
     # Start Telegram bot
     await attendance_bot.start()
